0baa3a4694d4106090bd27fad508007c724a602d
Enkang Yuan <yuan_enkang@outlook.com>
2024-04-19 14:57:22 -0500

    site refinements

--- src/components/HeaderMegaMenu.tsx
+++ src/components/HeaderMegaMenu.tsx
@@ -2,8 +2,16 @@ import { Group, Button, Box } from "@mantine/core";
 import classes from "../styles/HeaderMegaMenu.module.css";
 import logo from "../assets/gradient_02.svg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Burger } from "@mantine/core";
 
 export function HeaderMegaMenu() {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+
   return (
     <Box>
       <header className={classes.header}>
@@ -38,7 +46,50 @@ export function HeaderMegaMenu() {
               Join the waitlist
             </Button>
           </Group>
+          <Burger
+            hiddenFrom="sm"
+            opened={isDropdownVisible}
+            onClick={toggleDropdown}
+          />
         </Group>
+
+        {isDropdownVisible ? (
+          <div className={classes.fullScreenDiv}>
+            <a
+              href="/"
+              className={classes.dropdownlink}
+              style={{ fontWeight: "bold" }}
+            >
+              Home
+            </a>
+            <a
+              href="/contact"
+              className={classes.dropdownlink}
+              style={{ fontWeight: "bold" }}
+            >
+              Contact
+            </a>
+            <Button
+              variant="gradient"
+              gradient={{
+                from: "rgba(237, 189, 255, 1)",
+                to: "rgba(159, 202, 253, 0.5)",
+                deg: 210,
+              }}
+              component={Link}
+              to="/waitlist"
+              style={{
+                height: "70px",
+                width: "300px",
+                borderRadius: "999px",
+                fontSize: "24px",
+              }}
+              onClick={toggleDropdown}
+            >
+              Join the waitlist
+            </Button>
+          </div>
+        ) : null}
       </header>
     </Box>
   );
